73d26a5eef010aab9562cd2bdb1cc1f7139fb602
DanielleErni <glen.galit@betterask.erni>
2024-08-19 16:50:01 +0800

    Revert "applied seperation of concerns"

--- CodeSmellsRefactorExercise/Interface/IOperation.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace CodeSmellsRefactorExercise.Interface;
-
-public interface IOperation
-{
-    public double GetSum(List<int> numbers);
-    public double GetAverage(double sum, int count);
-}
